3dca558d27c99052b889268eddac88f299d13fca
haifa-02 <haifa.daoud@esprit.tn>
2018-06-24 18:51:19 +0200

    commit derniere version

--- src/souk/gui/ListeAnnonces.java
+++ src/souk/gui/ListeAnnonces.java
@@ -6,6 +6,7 @@ import com.codename1.io.NetworkEvent;
 import com.codename1.io.NetworkManager;
 import com.codename1.l10n.SimpleDateFormat;
 import com.codename1.ui.Button;
+import com.codename1.ui.ComboBox;
 import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.Dialog;
@@ -19,15 +20,20 @@ import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
 import com.codename1.ui.layouts.LayeredLayout;
+
 import com.codename1.ui.plaf.Style;
 import com.codename1.ui.spinner.Picker;
 import com.codename1.ui.util.Resources;
 
 import java.util.Date;
+
 import java.util.List;
+import java.util.Vector;
 
 import souk.entite.Annonces;
+import souk.entite.Categories;
 import souk.services.AnnoncesServices;
+import souk.services.CategorieService;
 import souk.util.SessionUser;
 
 /**
@@ -35,6 +41,8 @@ import souk.util.SessionUser;
  * @author HAYFA
  */
 public class ListeAnnonces extends BaseForm {
+
+    Container cntIndex = new Container();
 
     public ListeAnnonces(Resources res) {
         super("Annonces", BoxLayout.y(), res);
@@ -44,16 +52,24 @@ public class ListeAnnonces extends BaseForm {
 
         cntlbl.getAllStyles().setPadding(Component.TOP, 100);
         add(cntlbl);
+        Button ancAjouter = new Button("Ajouter");
+        cntIndex.add(ancAjouter);
+
+        ancAjouter.addActionListener((e) -> {
+            cntIndex.removeAll();
+            AjouterForm(res);
+            refreshTheme();
+        });
         ConnectionRequest con = new ConnectionRequest();
         con.setUrl("http://localhost:8000/api/annonces/all");
-        System.out.println("url : "+"http://localhost:8000/api/annonces/all");
+
         NetworkManager.getInstance().addToQueue(con);
         con.addResponseListener(new ActionListener<NetworkEvent>() {
 
             @Override
             public void actionPerformed(NetworkEvent evt) {
                 AnnoncesServices ser = new AnnoncesServices();
-                Container cntIndex = new Container();
+
                 List<Annonces> list = ser.getListAnnonces(new String(con.getResponseData()));
                 for (Annonces lst : list) {
                     addButton(res.getImage("large.jpg"), lst.getTitre(), lst.getDateCreation(), lst.getPrix(), cntIndex, lst.getId(), res);
@@ -85,7 +101,7 @@ public class ListeAnnonces extends BaseForm {
         btnDet.addActionListener((e) -> {
             cntIndex.setVisible(false);
             ConnectionRequest connection = new ConnectionRequest();
-            System.out.println("cedcbik" + idannonces);
+
             connection.setUrl("http://localhost:8000/api/annonces/all");
             NetworkManager.getInstance().addToQueue(connection);
             connection.addResponseListener(new ActionListener<NetworkEvent>() {
@@ -108,6 +124,7 @@ public class ListeAnnonces extends BaseForm {
             });
 
         });
+
         cnt.setUIID("Container");
         cnt.add(BorderLayout.CENTER,
                 BoxLayout.encloseY(
@@ -152,79 +169,139 @@ public class ListeAnnonces extends BaseForm {
 
         lblDescription.setUIID("TextFieldBlack");
         addStringValue(lbladresse, "Soukra");
-        
+
         String roles = SessionUser.getInstance().getRoles();
         System.out.println("roles" + roles);
         String client = "ROLE_CLIENT";
-        if(roles.indexOf(client)>=0 ){
+        if (roles.indexOf(client) >= 0) {
             Button cmd = new Button("Commander");
-            cmd.addActionListener((e)->{
+            cmd.addActionListener((e) -> {
 
+                Label lbl_date = new Label("Date :");
+                Label lbl_quantite = new Label("Quantite :");
+                TextField tf_date = new TextField();
+                TextField tf_quantite = new TextField();
+                Button btn_quitter = new Button("Quitter");
+                Button btn_valider = new Button("Valider");
+                tf_date.setHint("Date");
+                tf_quantite.setHint("Quantite");
 
-                    Label lbl_date = new Label("Date :");                       
-                    Label lbl_quantite = new Label("Quantite :");
-                    TextField tf_date = new TextField();
-                    TextField tf_quantite = new TextField();
-                    Button btn_quitter = new Button("Quitter"); 
-                    Button btn_valider = new Button("Valider");
-                    tf_date.setHint("Date"); 
-                    tf_quantite.setHint("Quantite");
-                    
-                    Dialog dlg = new Dialog("Nouvelle commande");
-                    dlg.setLayout(BoxLayout.y());
-                    Style dlgStyle = dlg.getDialogStyle();
-                    dlgStyle.setBgTransparency(255);
-                    dlgStyle.setBgColor(0xffffff);
-                    Picker datePicker = new Picker();
-                    datePicker.setType(Display.PICKER_TYPE_DATE);
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-                    datePicker.setFormatter(formatter);
-                    dlg.add(lbl_date);
-                    dlg.add(datePicker);
-                    dlg.add(lbl_quantite);
-                    dlg.add(tf_quantite);
+                Dialog dlg = new Dialog("Nouvelle commande");
+                dlg.setLayout(BoxLayout.y());
+                Style dlgStyle = dlg.getDialogStyle();
+                dlgStyle.setBgTransparency(255);
+                dlgStyle.setBgColor(0xffffff);
+                Picker datePicker = new Picker();
+                datePicker.setType(Display.PICKER_TYPE_DATE);
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+                datePicker.setFormatter(formatter);
+                dlg.add(lbl_date);
+                dlg.add(datePicker);
+                dlg.add(lbl_quantite);
+                dlg.add(tf_quantite);
 
-                    Button ok = new Button("Valider");
-                    ok.getAllStyles().setFgColor(0);
-                    dlg.add(ok);
-                    Button quitter = new Button("Quitter");
-                    quitter.getAllStyles().setFgColor(0);
-                    dlg.add(quitter);
-                    quitter.addActionListener((eq)->{
-                        dlg.setVisible(false);
-                        new CommandesPage(res).show();
-                        refreshTheme();
-                    });
-
-                    int id = SessionUser.getInstance().getId();
-                    ok.addActionListener((eq2)->{
-                        ConnectionRequest con = new ConnectionRequest();
-                        String d = datePicker.getText();
-                        String qt = tf_quantite.getText();
-                        con.setUrl("http://localhost:8000/api/commandes/new/"+idannonces+"/"+d+"/"+qt+"/"+id);
-                        NetworkManager.getInstance().addToQueue(con);
-                        con.addResponseListener(new ActionListener<NetworkEvent>() {
-                            @Override
-                            public void actionPerformed(NetworkEvent evt) {
-
-
-                                Dialog.show("Nouvelle Commande", "Commande ajoutée avec succès.", "OK",null);
-                                dlg.setVisible(false);
-
-                                new CommandesPage(res).show();
-                                refreshTheme();
-                            }
-                        });
-                    });
-                    dlg.showDialog();
-
-
-
+                Button ok = new Button("Valider");
+                ok.getAllStyles().setFgColor(0);
+                dlg.add(ok);
+                Button quitter = new Button("Quitter");
+                quitter.getAllStyles().setFgColor(0);
+                dlg.add(quitter);
+                quitter.addActionListener((eq) -> {
+                    dlg.setVisible(false);
+                    new CommandesPage(res).show();
+                    refreshTheme();
                 });
+
+                int id = SessionUser.getInstance().getId();
+                ok.addActionListener((eq2) -> {
+                    ConnectionRequest con = new ConnectionRequest();
+                    String d = datePicker.getText();
+                    String qt = tf_quantite.getText();
+                    con.setUrl("http://localhost:8000/api/commandes/new/" + idannonces + "/" + d + "/" + qt + "/" + id);
+                    NetworkManager.getInstance().addToQueue(con);
+                    con.addResponseListener(new ActionListener<NetworkEvent>() {
+                        @Override
+                        public void actionPerformed(NetworkEvent evt) {
+
+                            Dialog.show("Nouvelle Commande", "Commande ajoutée avec succès.", "OK", null);
+                            dlg.setVisible(false);
+
+                            new CommandesPage(res).show();
+                            refreshTheme();
+                        }
+                    });
+                });
+                dlg.showDialog();
+
+            });
             add(cmd);
         }
     }
-       
+
+    private void AjouterForm(Resources res) {
+
+        addStringValue("Categorie ", getListCategorie());
+
+        TextField txtprix = new TextField();
+
+        txtprix.setUIID("TextFieldBlack");
+        addStringValue("Prix ", txtprix);
+        TextField txtTitre = new TextField();
+
+        txtTitre.setUIID("TextFieldBlack");
+        addStringValue("Titre ", txtTitre);
+
+        TextArea txtDescription = new TextArea();
+
+        txtDescription.setUIID("TextAriaBlack");
+        addStringValue("Description ", txtDescription);
+
+        int id = SessionUser.getInstance().getId();
+        Button ancAjouter = new Button("Ajouter");
+        add(ancAjouter);
+        ancAjouter.addActionListener((e) -> {
+
+            ConnectionRequest con = new ConnectionRequest();
+            String categorie ="1";// getListCategorie().getSelectedItem().toString();
+            con.setUrl("http://localhost:8000/api/annonces/ajouter/" + id + "/" + txtprix.getText() + "/" + categorie + "/" + txtTitre.getText() + "/" + txtDescription.getText());
+            NetworkManager.getInstance().addToQueue(con);
+            con.addResponseListener(new ActionListener<NetworkEvent>() {
+                @Override
+                public void actionPerformed(NetworkEvent evt) {
+
+                    Dialog.show("Nouvelle annonce", "Annonces ajoutée avec succès.", "OK", null);
+                    refreshTheme();
+                }
+            });
+        });
+
+    }
+
+    private final ComboBox getListCategorie() {
+        ComboBox comboCategorie = new ComboBox();
+
+        ConnectionRequest con = new ConnectionRequest();
+
+        con.setUrl("http://localhost:8000/api/categorie/all");
+        NetworkManager.getInstance().addToQueue(con);
+        con.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+
+                CategorieService ser = new CategorieService();
+
+                List<Categories> list = ser.getListCategorie(new String(con.getResponseData()));
+
+                for (Categories lst : list) {
+
+                    comboCategorie.addItem(lst.getDesignation());
+                }
+            }
+        });
+
+        return comboCategorie;
+    }
+
     private void addStringValue(String s, Component v) {
         add(BorderLayout.west(new Label(s, "PaddedLabel")).
                 add(BorderLayout.CENTER, v));
