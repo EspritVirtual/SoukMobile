a3595c1e1c86a11405d92a81c1fd5a15fcadd657
soumayaZ <soumayaZ@users.noreply.github.com>
2018-06-28 16:57:24 +0100

    Commit:SZ:beforpush

--- src/souk/services/UserServices.java
+++ src/souk/services/UserServices.java
@@ -10,9 +10,14 @@ import com.codename1.io.ConnectionRequest;
 import com.codename1.io.JSONParser;
 import com.codename1.io.NetworkManager;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import souk.entite.Reclamations;
 import souk.entite.User;
 
 /**
@@ -53,6 +58,41 @@ public class UserServices {
         }
        // System.out.println(listEtudiants);
         return user;
+
+    }
+     
+     public ArrayList<User> getListUser(String json) {
+
+        ArrayList<User> lstuser = new ArrayList<>();
+
+        try {
+            System.out.println(json);
+            JSONParser j = new JSONParser();
+
+            Map<String, Object> coms = j.parseJSON(new CharArrayReader(json.toCharArray()));
+
+            List<Map<String, Object>> list = (List<Map<String, Object>>) coms.get("root");
+
+            for (Map<String, Object> obj : list) {
+               
+                
+                User user = new User();
+
+                float id = Float.parseFloat(obj.get("id").toString());
+                String roles = String.valueOf(obj.get("roles"));
+                user.setId((int) id);
+                if(obj.get("titreCommercial") != null)
+                user.setTitre(obj.get("titreCommercial").toString());
+                user.setUsername(obj.get("username").toString());
+                user.setRoles(roles);
+                lstuser.add(user);
+
+            }
+
+        } catch (IOException ex) {
+        }
+        System.out.println(lstuser);
+        return lstuser;
 
     }
 
